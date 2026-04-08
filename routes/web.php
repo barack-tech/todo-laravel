4a09495887e7dccca78fd2d2bec5a0f30f713100
barack-tech <barackotieno.tech@gmail.com>
2026-04-08 15:39:37 +0300

    feat: install Laravel Breeze for authentication

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,20 @@
 <?php
 
-use App\Http\Controllers\TaskController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{filter?}', [TaskController::class, 'index'])->name('tasks.index');
-Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
-Route::patch('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
-Route::patch('/tasks/{task}/toggle', [TaskController::class, 'toggle'])->name('tasks.toggle');
-Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
-// Routes are the entry point to an application, defining how requests are handled and which controller methods are invoked. In this code snippet, we define routes for a task management application, allowing users to view, create, update, toggle, and delete tasks. Each route is associated with a specific HTTP method and a controller action that processes the request and returns a response.
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
