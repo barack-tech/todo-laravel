b00dc842be20b534faff28c7a030711e681734fa
barack-tech <barackotieno.tech@gmail.com>
2026-04-08 12:11:32 +0300

    add coments

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,4 @@ Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
 Route::patch('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
 Route::patch('/tasks/{task}/toggle', [TaskController::class, 'toggle'])->name('tasks.toggle');
 Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+// Routes are the entry point to an application, defining how requests are handled and which controller methods are invoked. In this code snippet, we define routes for a task management application, allowing users to view, create, update, toggle, and delete tasks. Each route is associated with a specific HTTP method and a controller action that processes the request and returns a response.
