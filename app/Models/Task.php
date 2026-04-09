6f2c39d2667981e066726015f1665cd6749303f6
barack-tech <barackotieno.tech@gmail.com>
2026-04-09 16:55:52 +0300

    scope tasks to authenticated user

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -21,4 +21,3 @@ class Task extends Model
         return $this->belongsTo(User::class);
     }
 }
-// Models represent the data structure of an application and provide an interface for interacting with the database. In this code snippet, we define a Task model that has two attributes: 'task' (a string representing the task description) and 'done' (a boolean indicating whether the task is completed). The $fillable property specifies which attributes can be mass-assigned, while the $casts property ensures that the 'done' attribute is treated as a boolean when retrieved from the database.
--- app/Models/User.php
+++ app/Models/User.php
@@ -48,7 +48,7 @@ class User extends Authenticatable
     }
 
     public function tasks()
-{
+    {
     return $this->hasMany(Task::class);
 }
 }
