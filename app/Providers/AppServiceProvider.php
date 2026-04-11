8259b0f2adb51cdc9a16d3cf49d07069ecb9504b
barack-tech <barackotieno.tech@gmail.com>
2026-04-11 19:32:46 +0300

    feat: added pagination 20 tasks per page

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,19 @@
-use Illuminate\Pagination\Paginator;
+<?php
 
-public function boot(): void
+namespace App\Providers;
+
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
 {
-    Paginator::useTailwind();
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        Paginator::useTailwind();
+    }
 }
