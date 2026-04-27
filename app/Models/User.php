92e176046b99a7988a896b559a2fd5ce65147d00
barack-tech <barackotieno.tech@gmail.com>
2026-04-27 23:57:53 +0300

    feat: activate email verification for users

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use App\Models\Task;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Auth\MustVerifyEmail
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
